9e6cee38fc4f6bd2b61bd331b22667812ebeccd0
Prashanth Puranik <puranik@digdeeper.in>
2021-06-28 23:21:50 +0530

    first commit

---
+++ src/components/WorkshopsList.vue
@@ -0,0 +1,38 @@
+<template>
+    <div>
+        <h1>List of Workshops</h1>
+        <hr />
+        <ol>
+            <li v-for="workshop in workshops" :key="workshop._id">
+                <h2>{{workshop.name}}</h2>
+                <div>Speaker: {{workshop.speaker}}</div>
+            </li>
+        </ol>
+    </div>
+</template>
+
+<script>
+    import axios from 'axios';
+    
+    export default {
+        name: 'WorkshopsList',
+        data() {
+            return {
+                workshops: []
+            };
+        },
+        created() {
+            axios.get( `http://localhost:3000/api/workshops` )
+                .then(response => {
+                    this.workshops = response.data;
+                    console.log( this.workshops );
+                })
+                .catch(error => {
+                    alert( error.message )
+                });
+        }
+    };
+</script>
+
+<style scoped>
+</style>
